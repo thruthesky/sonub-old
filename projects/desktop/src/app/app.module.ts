8adb862f792117efd51fd9fd7f27210a3a0766e9
JAEHO SONG <thruthesky@gmail.com>
2018-09-19 16:48:33 +0800

    refactoring app structure

--- projects/desktop/src/app/app.module.ts
+++ projects/desktop/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LeftComponent } from './components/left/left.component';
+import { RightComponent } from './components/right/right.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LeftComponent,
+    RightComponent
   ],
   imports: [
     BrowserModule
