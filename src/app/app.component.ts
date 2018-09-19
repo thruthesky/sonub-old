77a47ec2be04cd7cd58cf0e5460bbce7d5f19db8
JAEHO SONG <thruthesky@gmail.com>
2018-09-19 22:08:00 +0800

    refactoring layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-layout';
+
+  @HostListener('scroll', ['$event'] ) onScroll(event: Event) {
+    console.log(event);
+  }
+  @HostListener('click', ['$event'] ) onClick(event: Event) {
+    console.log(event);
+  }
 }
