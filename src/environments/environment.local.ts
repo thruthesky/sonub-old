3074a2a14067a9fcc820e12c09f77e56b8e8e7ba
JAEHO SONG <thruthesky@gmail.com>
2018-09-19 15:43:21 +0800

    removing ionic codes

--- src/environments/environment.local.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    philgoServerUrl: 'http://192.168.0.254/sapcms_1_2/api.php',
-    philgoFileServerUrl: 'http://192.168.0.254/sapcms_1_2/api.php',
-    newFileServerUrl: 'http://192.168.0.254/file-server/index.php',
-};
--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  philgoServerUrl: 'https://www.philgo.com/api.php',
-  philgoFileServerUrl: 'https://file.philgo.com/api.php',
-  newFileServerUrl: 'https://file.philgo.com/~file_server/index.php'
-};
