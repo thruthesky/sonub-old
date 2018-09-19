6549b4b1b5ddda0a834878a37b7d67f506789a8d
JAEHO SONG <thruthesky@gmail.com>
2018-09-19 17:54:51 +0800

    update for layout

--- projects/desktop/src/app/components/left/left.component.ts
+++ projects/desktop/src/app/components/left/left.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-left',
   templateUrl: './left.component.html',
-  styleUrls: ['./left.component.css']
+  styleUrls: ['./left.component.scss']
 })
 export class LeftComponent implements OnInit {
 
--- projects/mobile/src/app/app.module.ts
+++ projects/mobile/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+
+import {MatToolbarModule, MatButtonModule, MatCheckboxModule} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule, MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
