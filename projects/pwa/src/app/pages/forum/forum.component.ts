a7b5ed1fc82f9617451fe592726d2d42b8e63ff9
JAEHO SONG <thruthesky@gmail.com>
2018-09-20 23:26:38 +0800

    update work on post list

--- projects/pwa/src/app/pages/forum/forum.component.ts
+++ projects/pwa/src/app/pages/forum/forum.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from '../../../services/app.service';
-import { PhilGoApiService, ApiPostSearch } from 'share/philgo-api/philgo-api.service';
+import { PhilGoApiService, ApiPostSearch, ApiPostQueryOptions } from 'share/philgo-api/philgo-api.service';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -11,6 +11,7 @@ import { ActivatedRoute } from '@angular/router';
 export class ForumComponent implements OnInit {
 
   post_id;
+  page_no = 1;
   res: ApiPostSearch;
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -18,13 +19,20 @@ export class ForumComponent implements OnInit {
     public philgo: PhilGoApiService
   ) {
     activatedRoute.paramMap.subscribe(params => {
+
       this.post_id = params.get('post_id');
-      philgo.postSearch({ post_id: this.post_id }).subscribe(res => {
+      const options: ApiPostQueryOptions = {};
+      if (this.page_no === 1) {
+        options.cache = true;
+        options.cacheId = `cache-first-list-of-${this.post_id}`;
+      }
+      philgo.postSearch({ post_id: this.post_id }, options).subscribe(res => {
         console.log('res: ', res);
         this.res = res;
       }, e => {
         console.log(e);
       });
+
     });
   }
 
