f5e57466556e7a7dafe8307bc63ba3233ee0907c
JAEHO SONG <thruthesky@gmail.com>
2018-09-27 19:26:15 +0800

    addign group id

--- projects/pwa/src/app/pages/job/job.component.ts
+++ projects/pwa/src/app/pages/job/job.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from '../../../services/app.service';
 
 @Component({
   selector: 'app-job',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JobComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public a: AppService
+  ) { }
 
   ngOnInit() {
   }
--- projects/pwa/src/environments/environment.prod.ts
+++ projects/pwa/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   port: '',
   blogIntroDomain: 'intro.sonub.com',
-  philgoServerUrl: 'http://192.168.0.254/sapcms_1_2/api.php',
-  philgoFileServerUrl: 'http://192.168.0.254/sapcms_1_2/api.php',
-  newFileServerUrl: 'http://192.168.0.254/file-server/index.php',
+  philgoServerUrl: 'https://www.philgo.com/api.php',
+  philgoFileServerUrl: 'https://file.philgo.com/api.php',
+  newFileServerUrl: 'https://file.philgo.com/~file_server/index.php',
 };
