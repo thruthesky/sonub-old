c03d55589a6cbd644d4f89110f4ff5dd17139e88
lancelynyrd <pinedaclp@gmail.com>
2018-09-27 17:17:08 +0800

    add local environment

---
+++ projects/pwa/src/environments/environment.local.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    envName: 'local',
+    port: '',
+    blogIntroDomain: 'intro.sonub.com',
+    philgoServerUrl: 'http://192.168.0.254/sapcms_1_2/api.php',
+    philgoFileServerUrl: 'http://192.168.0.254/sapcms_1_2/api.php',
+    newFileServerUrl: 'http://192.168.0.254/file-server/index.php',
+};
