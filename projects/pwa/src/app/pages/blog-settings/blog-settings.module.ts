e887208c8c5012e1aa785a5b5e1667a405ecb650
JAEHO SONG <thruthesky@gmail.com>
2018-09-28 18:34:57 +0800

    refactoring blog settings page. separating each sections

--- projects/pwa/src/app/pages/blog-settings/blog-settings.module.ts
+++ projects/pwa/src/app/pages/blog-settings/blog-settings.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BlogSettingsComponent } from './blog-settings.component';
 import { RouterModule, Routes } from '@angular/router';
-import { MatFormFieldModule, MatInputModule, MatButtonModule, MatToolbarModule } from '@angular/material';
+import { MatFormFieldModule, MatInputModule, MatButtonModule, MatToolbarModule, MatListModule } from '@angular/material';
 import { FormsModule } from '@angular/forms';
 
 
@@ -19,6 +19,7 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     RouterModule.forChild(routes),
+    MatListModule,
     MatToolbarModule,
     MatFormFieldModule,
     MatInputModule,
