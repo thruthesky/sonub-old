f37dabaf818763be25a779fda7c459442e00165f
JAEHO SONG <thruthesky@gmail.com>
2018-10-03 16:03:22 +0800

    adding sitemaps

--- projects/pwa/src/services/stat.service.ts
+++ projects/pwa/src/services/stat.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, } from 'rxjs';
 import { environment } from '../environments/environment';
+import { map } from 'rxjs/operators';
+import { SimpleLibrary as _ } from 'ng-simple-library';
+
 
 interface PageView {
     function?: string | number;
@@ -15,7 +18,7 @@ interface PageView {
     data?: any;
 }
 
-@Injectable({ providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class StatService {
     constructor(
         private http: HttpClient
@@ -23,9 +26,20 @@ export class StatService {
 
     }
 
-    getPageView(req: PageView): Observable<PageView> {
-        req['function'] = 'pageView';
-        const params = new HttpParams( { fromObject: <any> req } );
-        return <any> this.http.get( environment.sonubLogApiServerUrl, { params: params } );
+    getData(req: PageView): Observable<PageView> {
+        const params = new HttpParams({ fromObject: <any>req });
+
+        console.log('getData from: ', environment.sonubLogApiServerUrl + '?' + _.httpBuildQuery(req) );
+        return <any>this.http.get(environment.sonubLogApiServerUrl, { params: params })
+            .pipe(
+                map(res => {
+                    console.log('res: ', res);
+                    if (res && res['code'] && res['code'] < 0) {
+                        throw res;
+                    } else {
+                        return res;
+                    }
+                })
+            );
     }
 }
