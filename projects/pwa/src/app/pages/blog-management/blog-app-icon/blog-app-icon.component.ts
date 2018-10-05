9422643f246fc46098c24f3b77d94aa4fabf5a64
JAEHO SONG <thruthesky@gmail.com>
2018-10-05 23:08:11 +0800

    generating thumbnails

--- projects/pwa/src/app/pages/blog-management/blog-app-icon/blog-app-icon.component.ts
+++ projects/pwa/src/app/pages/blog-management/blog-app-icon/blog-app-icon.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiBlogSettings, PhilGoApiService } from 'share/philgo-api/philgo-api.service';
+import { ApiBlogSettings, PhilGoApiService, ApiThumbnailGenerate } from 'share/philgo-api/philgo-api.service';
 import { AppService } from '../../../../services/app.service';
 
 @Component({
@@ -10,6 +10,7 @@ import { AppService } from '../../../../services/app.service';
 export class BlogAppIconComponent implements OnInit {
 
   blog: ApiBlogSettings = <any>{};
+  percentage = 0;
   constructor(
     public a: AppService,
     public philgo: PhilGoApiService
@@ -45,6 +46,66 @@ export class BlogAppIconComponent implements OnInit {
     return false;
   }
 
+
+  onChangeFile(event: Event, code: string) {
+    const files = <any>event.target['files'];
+
+    if (files === void 0 || !files.length || files[0] === void 0) {
+      const e = { code: -1, content: this.philgo.t({ en: 'Please select a file', ko: '업로드 할 파일을 선택해주세요.' }) };
+      return;
+    }
+
+    this.philgo.fileUpload(files, { gid: this.a.philgo.myIdx(), code: code }).subscribe(res => {
+      if (typeof res === 'number') {
+        console.log('percentage: ', res);
+        this.percentage = res;
+      } else {
+        console.log('file success: ', res);
+        this.blog.app_url_icons_src_512 = res.src;
+        this.percentage = 0;
+        this.generateAppIconThumbnails(res.path, 0);
+      }
+    }, e => {
+      console.error(e);
+      this.a.error(e);
+      this.percentage = 0;
+    });
+  }
+
+  /**
+   * Generate thumbnails
+   * @param path uploaded file's data path
+   * @param count number to generate thumbnails
+   */
+  generateAppIconThumbnails(path: string, count: number) {
+    const sizes = [
+      { width: 72, height: 72},
+      { width: 96, height: 96},
+      { width: 128, height: 128},
+      { width: 144, height: 144},
+      { width: 152, height: 152},
+      { width: 192, height: 192},
+      { width: 384, height: 384},
+      { width: 512, height: 512},
+    ];
+    const options: ApiThumbnailGenerate = {
+      path: path,
+      prefix: 'icons-' + this.philgo.myIdx(),
+      width: sizes[count].width,
+      height: sizes[count].height,
+      mode: 'adaptive',
+      ext: 'png',
+    };
+    this.philgo.generateThumbnail(options).subscribe(thumb64 => {
+      console.log('thumbnail generate', thumb64);
+      if ( count >= 7 ) {
+        return;
+      } else {
+        count++;
+        this.generateAppIconThumbnails( path, count);
+      }
+    }, e => this.a.error(e));
+  }
 
 
 }
