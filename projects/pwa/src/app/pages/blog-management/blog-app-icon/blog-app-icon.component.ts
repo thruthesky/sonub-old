a68374b394bdfb9c6bb9624813d2104c3240d12d
JAEHO SONG <thruthesky@gmail.com>
2018-10-05 20:58:26 +0800

    update for blog settings

--- projects/pwa/src/app/pages/blog-management/blog-app-icon/blog-app-icon.component.ts
+++ projects/pwa/src/app/pages/blog-management/blog-app-icon/blog-app-icon.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiBlogSettings, PhilGoApiService } from 'share/philgo-api/philgo-api.service';
+import { AppService } from '../../../../services/app.service';
 
 @Component({
   selector: 'app-blog-app-icon',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogAppIconComponent implements OnInit {
 
-  constructor() { }
+  blog: ApiBlogSettings = <any>{};
+  constructor(
+    public a: AppService,
+    public philgo: PhilGoApiService
+  ) { }
 
   ngOnInit() {
   }
+
+
+  onSubmit($event: Event) {
+    $event.preventDefault();
+
+    console.log('req: ', this.blog);
+    const data: ApiBlogSettings = <any>{};
+    data.name = this.blog.app_name;
+    data.app_short_name = this.blog.app_short_name;
+    data.app_theme_color = this.blog.app_theme_color;
+    this.philgo.blogSaveSettings(data).subscribe(res => {
+      console.log('blog saved: ', res);
+      this.a.toast(this.a.t({ en: 'Blog settings have successfully updated.', ko: '블로그 설정이 저장되었습니다.' }));
+      // this.philgo.profile().subscribe(r => {
+      //   console.log('blog save => user local storage updated to restore blog domain: ', r);
+      // });
+    }, e => this.a.error(e));
+
+
+    return false;
+  }
+
+
 
 }
