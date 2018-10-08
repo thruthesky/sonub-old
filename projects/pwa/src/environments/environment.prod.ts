4ffea6658be5fc6cac991a53bb741700cba1df9e
lancelynyrd <pinedaclp@gmail.com>
2018-10-08 21:40:03 +0800

    update on the statistics

--- projects/pwa/src/environments/environment.prod.ts
+++ projects/pwa/src/environments/environment.prod.ts
@@ -5,6 +5,8 @@ export const environment = {
   philgoServerUrl: 'https://www.philgo.com/api.php',
   philgoFileServerUrl: 'https://file.philgo.com/api.php',
   newFileServerUrl: 'https://file.philgo.com/~file_server/index.php',
-  sonubLogServerUrl: 'https://production.sonub.com:4431/',
-  sonubLogApiServerUrl: 'https://production.sonub.com/sonub-supporting-server/index.php'
+  // sonubLogServerUrl: 'https://production.sonub.com:4431/',
+  // sonubLogApiServerUrl: 'https://production.sonub.com/sonub-supporting-server/index.php'
+  sonubLogServerUrl: 'https://www.sonub.com:4431/',
+  sonubLogApiServerUrl: 'https://www.sonub.com/sonub-supporting-server/index.php'
 };
